09759d1f54e793f87cb74fe29b5d8f39d990f958
Aman saxena <asaxena7531@gmail.com>
2022-09-06 15:41:30 +0530

    mirror changes in navbar

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,7 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+def mycart(request):
+    return HttpResponse("this is my chart")
