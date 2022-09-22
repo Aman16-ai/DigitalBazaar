bb16ee303197cbe4cfba97e12372472e86b4266a
Aman saxena <asaxena7531@gmail.com>
2022-09-22 20:05:42 +0530

    carsouel

--- product/views.py
+++ product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from product.models import Product
 # Create your views here.
+def index(request,pk):
+    product = Product.objects.get(pk = pk)
+    print(product)
+    return HttpResponse(pk)
