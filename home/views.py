23df20c78ef7079b6a18e8bd149e5112b39f869a
Aman saxena <asaxena7531@gmail.com>
2022-09-23 19:48:25 +0530

    men's wear

--- home/views.py
+++ home/views.py
@@ -1,20 +1,19 @@
 from tkinter import EW
+from unicodedata import category
 from django.http import HttpResponse
 from django.shortcuts import render
-from product.models import Product
+from product.models import Category, Product
 
 # Create your views here.
 def index(request):
     products = Product.objects.all()
     
+    mensCategory = Category.objects.get(name="Men's wear")
+    mens_wear = Product.objects.filter(category = mensCategory)
     products_with_highest_discount = products[0]
     for p in products:
         if p.discount >= products_with_highest_discount.discount:
             products_with_highest_discount = p
             
-    
-            
-    print(products_with_highest_discount)
-    print(Product.getDiscountedPrice())
-    context = {"bestDeal":products_with_highest_discount,"discounted_products":Product.getDiscountedPrice()}
+    context = {"bestDeal":products_with_highest_discount,"discounted_products":Product.getDiscountedProduct(),"mens":mens_wear}
     return render(request,'index.html',context)
