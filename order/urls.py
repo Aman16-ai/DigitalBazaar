cbfc3781d83423d34eef595c37ae8b613e5b8fdf
aman <asaxena7531@gmail.com>
2023-12-10 21:09:47 +0530

    decouple the razorpay api from orderapi

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
 from .router import order_router
+from .views import CreateRazorPayOrderId
 
 urlpatterns = [
     path("",include(order_router.urls)),
     # path("transcation/",include(tran_router.urls))
+    path("createOrderId",view=CreateRazorPayOrderId.as_view(),name="createOrderId")
 ]
